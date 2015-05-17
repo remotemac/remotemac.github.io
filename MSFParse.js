51eada3fc8311de3701af277096fc982b371e752
bestibetgroup <bestibetgroup@gmail.com>
2015-05-17 10:51:23 -0400

    *Hopefully Final* Commit

--- MSFParse.js
+++ MSFParse.js
@@ -17,7 +17,8 @@ function signUpUser() {
 		        success: function(user) {
 			        alert("You successfully signed up!");
 			        var currentUser = Parse.User.current();  
-			        	if(currentUser) {
+			        if(currentUser) {
+
 	  					// TODO: Bring them to the hidden "logged in only" page
   					}  
 			     },
@@ -31,12 +32,13 @@ function signOut(){
 	window.location.href = "index.html";
 
 }
+
 function openSetup(){
 
 	window.location.href = "setup.html";
 	
-
 }
+
 function openCommands(){
 
 	window.location.href = "commands.html";
@@ -46,6 +48,49 @@ function openCommands(){
 function openHome() {
 
 	window.location.href = "main.html";
+
+}
+
+function openNewCommand() {
+
+	window.location.href = "newcommand.html";
+
+}
+
+function downloadRbFile() {
+
+	var finishedScript = "";
+	finishedScript += "module Commands\n";
+	finishedScript += "  class " + document.getElementById("inputName").value + "\n";
+	finishedScript += "    def initialize(args, settings)\n";
+	finishedScript += "    end\n";
+	finishedScript += "    def go\n";
+	finishedScript += "    " + document.getElementById("inputScript").value + "\n";
+	finishedScript += "    end\n";
+	finishedScript += "    def respond\n";
+	finishedScript += "    " + document.getElementById("inputResponse").value + "\n";
+	finishedScript += "    end\n";
+	finishedScript += "    def media?\n";
+	finishedScript += "    " + document.getElementById("hasMedia").value + "\n";
+	finishedScript += "    end\n";
+	finishedScript += "    def self.matches\n";
+	finishedScript += "    " + document.getElementById("inputMatches").value + "\n";
+	finishedScript += "    end\n";
+	finishedScript += "  end\n";
+	finishedScript += "end\n";
+
+	var pom = document.createElement("a");
+	pom.setAttribute("href","data:text/plain;charset=utf-8," + encodeURIComponent(finishedScript));
+	pom.setAttribute("download", document.getElementById("inputName").value + ".rb");
+	
+	pom.style.display = "none";
+
+	document.body.appendChild(pom);
+	pom.click();
+	document.body.removeChild(pom);
+
+	console.log(finishedScript);
+
 }
 /*
 function openMyShoppingList(){
@@ -70,6 +115,7 @@ function signInUser() {
   					//alert("You successfully logged in!");
   					var currentUser = Parse.User.current();
   					if(currentUser) {
+  						document.cookie = "number=" + username + ";";
 	  					window.location.href = "main.html";
   					}  
   				},
